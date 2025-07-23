e660b2cd8b41e11052aef2a8b4e95bdcad6bc6f9
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-23 19:12:59 +0200

    Fetching Lists of Posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ tittle: "First Post", content: "This is the first post!" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/duck.jpg",
+        creator: {
+          name: "Adhurim",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
@@ -8,10 +19,8 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   // Create post in db
-  res
-    .status(201)
-    .json({
-      message: "Post created successfully!",
-      post: { id: new Date().toISOString(), title: title, content: content },
-    });
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
 };
