6c2f7b9ae6313cb74537643c78abe06a4bdc643c
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-24 22:58:25 +0200

    Logging In & JWTs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -44,18 +45,28 @@ exports.login = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        const error = new Error("A user with this email could not be found");
+        const error = new Error("A user with this email could not be found.");
         error.statusCode = 401;
         throw error;
       }
       loadedUser = user;
-      return bcrypt.compare(password, user.password).then((isEqual) => {
-        if (!isEqual) {
-          const error = new error("Wrong password!");
-          error.statusCode = 401;
-          throw error;
-        }
-      });
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong password!");
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        "somesupersecretsecret",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch((err) => {
       if (!err.statusCode) {
