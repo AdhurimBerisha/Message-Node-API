cdb4191f02856fe96f7f3eedad1e725734ac8538
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-24 23:29:06 +0200

    Authorization Checks for Update & Delete

--- controllers/feed.js
+++ controllers/feed.js
@@ -124,6 +124,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error("Not authorized!");
+        error.statusCode = 403;
+        throw error;
+      }
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -153,6 +158,11 @@ exports.deletePost = (req, res, next) => {
         throw error;
       }
       // check logged in user
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error("Not authorized!");
+        error.statusCode = 403;
+        throw error;
+      }
       clearImage(post.imageUrl);
       return Post.findByIdAndDelete(postId);
     })
