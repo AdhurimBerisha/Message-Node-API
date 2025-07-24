6c30233cd0e448846cea427fb302eaa93dbf18a4
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-24 14:20:12 +0200

    Updating Posts

--- app.js
+++ app.js
@@ -41,12 +41,9 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE"
+    "GET, POST, PUT, PATCH, DELETE"
   );
+
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Content-Type",
