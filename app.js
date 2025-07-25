d15e287c23311593dedc43be1548761cc585ba9c
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-07-25 15:41:19 +0200

    Connection fix for Socket.io

--- app.js
+++ app.js
@@ -65,7 +65,13 @@ mongoose
   )
   .then((result) => {
     const server = app.listen(8080);
-    const io = require("socket.io")(server);
+    const io = require("socket.io")(server, {
+      cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true,
+      },
+    });
     io.on("connection", (socket) => {
       console.log("Client connected");
     });
